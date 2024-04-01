7a1a97d19e3dd6f96b70fd15e0c13b7bbb408430
andrey_kir <andrey.kirillov.kiev@gmail.com>
2024-04-01 16:46:32 +0300

    init executeInstruction

--- src/rp2040.ts
+++ src/rp2040.ts
@@ -16,13 +16,14 @@ class RP2040 {
    * 16 MB
    * starts at 0x10000000
    */
-  private flash =  new Uint8Array( 16 * 1024 * 1024);
-  private flash16 =  new Uint16Array(this.flash.buffer);
+  private flash =  new Uint8Array( 16 * 1024 * 1024); // byte size
+  private flash16 =  new Uint16Array(this.flash.buffer);   // word size (2 x byte)
 
   /**
    * 16 registers
    */
   private registers = new Uint32Array(16);
+
   /**
    * ROM
    */
@@ -46,7 +47,12 @@ class RP2040 {
 
   executeInstruction() {
     const opcode = this.flash16[this.PC / 2];
-    console.log('opcode', opcode, opcode.toString(16));
+    // console.log('opcode', {
+    //   opcode,
+    //   opcodeHex: opcode.toString(16),
+    //   flash16: this.flash16,
+    //   flash: this.flash,
+    // });
   }
 }
 
@@ -57,3 +63,4 @@ const hex = fs.readFileSync('src/utils/blink.hex', 'utf-8');
 const mcu = new RP2040(hex);
 mcu.executeInstruction();
 
+console.log('\x1b[36m%s\x1b[0m', 'BYE RP2040!');
--- src/utils/intelHex.ts
+++ src/utils/intelHex.ts
@@ -4,9 +4,23 @@ export function loadHex(source: string, target: Uint8Array) {
       const bytes = parseInt(line.substr(1, 2), 16);
       const addr = parseInt(line.substr(3, 4), 16);
 
+      // console.log('loadHex', {
+      //   line,
+      //   bytes,
+      //   addr,
+      // });
+
       for (let i = 0; i < bytes; i++) {
         target[addr + i] = parseInt(line.substr(9 + i * 2, 2), 16);
+        // console.log('loadHex', {
+        //   index: addr + i,
+        //   dataString: line.substr(9 + i * 2, 2)
+        // });
       }
     }
   }
+
+  // console.log('loadHex', {
+  //   target,
+  // })
 }
