2b18ee73d67fa59322aad0efa4973536c35b3d1a
andrey_kir <andrey.kirillov.kiev@gmail.com>
2024-04-01 19:11:13 +0300

    get push opcode at start of the main fn

--- src/rp2040.ts
+++ src/rp2040.ts
@@ -1,26 +1,31 @@
-import { loadHex } from './utils/intelHex';
 import * as fs from 'fs';
 
-console.log('\x1b[36m%s\x1b[0m', 'HELLO RP2040!');
+import { loadHex } from './utils/intelHex';
+import COLORS from './utils/nodeColors';
+
+
+console.log(COLORS.fg.blue, 'HELLO RP2040!');
 
 class RP2040 {
+  /** ------------------------------------------ Addresses ---------------------------------------------------------- */
+
   /**
    * SRAM
    * 264 KB
    * starts at 0x20000000
    */
-  private sram =  new Uint8Array( 264 * 1024);
+  private sram = new Uint8Array(264 * 1024);
 
   /**
    * FLASH
    * 16 MB
    * starts at 0x10000000
    */
-  private flash =  new Uint8Array( 16 * 1024 * 1024); // byte size
-  private flash16 =  new Uint16Array(this.flash.buffer);   // word size (2 x byte)
+  private flash = new Uint8Array(16 * 1024 * 1024); // byte size
+  private flash16 = new Uint16Array(this.flash.buffer);   // word size (2 x byte)
 
   /**
-   * 16 registers
+   * 16
    */
   private registers = new Uint32Array(16);
 
@@ -32,11 +37,19 @@ class RP2040 {
    * APB Peripherals
    */
 
+  /** ---------------------------------------- Constructor ---------------------------------------------------------- */
   constructor(hex: string) {
     // console.log('loading hex\n', hex);
+    this.SP = 0x20041000;
+    this.flash.fill(0xff);
     loadHex(hex, this.flash);
   }
 
+  /** ------------------------------------------ Registers ---------------------------------------------------------- */
+
+  /**
+   * Program counter
+   */
   get PC() {
     return this.registers[15];
   }
@@ -45,22 +58,52 @@ class RP2040 {
     this.registers[15] = value;
   }
 
+  /**
+   * Stack pointer
+   */
+  get SP() {
+    return this.registers[13];
+  }
+
+  set SP(value: number) {
+    this.registers[13] = value;
+  }
+
+  /**
+   * Link register
+   */
+  get LR() {
+    return this.registers[14];
+  }
+
+  set LR(value: number) {
+    this.registers[14] = value;
+  }
+
+  /** ------------------------------------------ Methods ------------------------------------------------------------ */
   executeInstruction() {
     const opcode = this.flash16[this.PC / 2];
-    // console.log('opcode', {
-    //   opcode,
-    //   opcodeHex: opcode.toString(16),
-    //   flash16: this.flash16,
-    //   flash: this.flash,
-    // });
+
+    if ((opcode >> 9) === 0b1011010) {
+      console.log(COLORS.fg.green, 'instruction: push');
+    }
+
+    console.log('opcode', {
+      opcode,
+      opcodeBin: opcode.toString(2).replace(/^(.{8})/,"$1 "),
+      opcodeHex: opcode.toString(16),
+      // flash16: this.flash16,
+      // flash: this.flash,
+    });
   }
 }
 
-/** ---------------------------------------------------------------------------------------------------------------- */
+/** ----------------------------------------------- Test ------------------------------------------------------------ */
 
 const hex = fs.readFileSync('src/utils/blink.hex', 'utf-8');
 
 const mcu = new RP2040(hex);
+mcu.PC = 0x304;
 mcu.executeInstruction();
 
-console.log('\x1b[36m%s\x1b[0m', 'BYE RP2040!');
+console.log(COLORS.fg.blue, 'BYE RP2040!');
